e877f7a9e341fd1484db53cb391660977b37d1ce
Hayden Chen <brooklyn1984@qq.com>
2023-04-26 13:35:33 +0800

    refactor: :recycle: refactor preview

--- src/app/Preview.tsx
+++ src/app/Preview.tsx
@@ -1,17 +1,27 @@
 import Preview from "@/components/Preview";
+import { useEffect } from "react";
 
 export type Mode = "edit" | "preview";
 
 const PreviewApp: React.FC<{ pageName: string }> = ({ pageName }) => {
+  useEffect(() => {
+    const close = (e) => {
+      if (e.key === "Escape") logseq.hideMainUI();
+    };
+    // listen esc
+    window.addEventListener("keyup", close);
+    return () => {
+      window.removeEventListener("keyup", close);
+    };
+  }, []);
+
   return (
     <div className="w-screen h-screen flex items-center justify-center overflow-auto">
       <div
-        className="w-screen h-screen fixed top-0 left-0 bg-black opacity-50"
+        className="w-screen h-screen fixed top-0 left-0 bg-black opacity-70"
         onClick={() => logseq.hideMainUI()}
       ></div>
-      <div className="w-4/5 h-4/5 z-10 overflow-auto">
-        <Preview pageName={pageName} />
-      </div>
+      <Preview pageName={pageName} />
     </div>
   );
 };
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import { getExcalidrawInfoFromPage } from "../helper/util";
 import { exportToSvg } from "@excalidraw/excalidraw";
+import { TbZoomInFilled, TbZoomOutFilled } from "react-icons/tb";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const Preview: React.FC<React.PropsWithChildren<{ pageName: string }>> = ({
   pageName,
@@ -15,14 +17,37 @@ const Preview: React.FC<React.PropsWithChildren<{ pageName: string }>> = ({
           appState: excalidrawData?.appState ?? {},
           files: excalidrawData?.files ?? null,
         });
+        const width = Number(svg.getAttribute("width")) || 100;
+        const height = Number(svg.getAttribute("height")) || 80;
         if (containerRef?.current) {
+          // display svg in full screen based on aspect radio
+          const aspectRadio = width / height;
+          const windowAspectRadio = window.innerWidth / window.innerHeight;
+          if (aspectRadio > windowAspectRadio) {
+            svg.style.width = "100vw";
+            svg.style.height = "auto";
+          } else {
+            svg.style.width = "auto";
+            svg.style.height = "100vh";
+          }
+
           containerRef.current.appendChild(svg);
         }
       });
     }
   }, [pageName]);
 
-  return <div ref={containerRef}></div>;
+  return (
+    <>
+      <div ref={containerRef} className="z-10 max-w-[100vw]"></div>
+      <div className="fixed top-5 right-5 text-2xl z-20">
+        <AiFillCloseCircle
+          className="cursor-pointer"
+          onClick={() => logseq.hideMainUI()}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Preview;
