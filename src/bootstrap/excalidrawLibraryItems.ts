7c9e9cd2947f6a00edbf7f3afe4351c37e93232c
Hayden Chen <brooklyn1984@qq.com>
2023-08-21 13:14:50 +0800

    chore: :wrench: add @haydenull/fabric

--- src/bootstrap/excalidrawLibraryItems.ts
+++ src/bootstrap/excalidrawLibraryItems.ts
@@ -1,41 +1,36 @@
-import {
-  DEFAULT_EXCALIDRAW_LIBRARY_ITEMS,
-  EXCALIDRAW_FILE_PROMPT,
-} from "@/lib/constants";
-import { genBlockData, getExcalidrawData } from "@/lib/utils";
-import { LibraryItems } from "@excalidraw/excalidraw/types/types";
+import type { LibraryItems } from '@excalidraw/excalidraw/types/types'
 
-const PAGE_NAME = "excalidraw-library-items-storage";
+import { DEFAULT_EXCALIDRAW_LIBRARY_ITEMS, EXCALIDRAW_FILE_PROMPT } from '@/lib/constants'
+import { genBlockData, getExcalidrawData } from '@/lib/utils'
+
+const PAGE_NAME = 'excalidraw-library-items-storage'
 /**
  * initialize the page sorted in the excalidraw library items
  */
 const bootExcalidrawLibraryItems = async () => {
-  const page = await logseq.Editor.getPage(PAGE_NAME);
-  if (page) return;
+  const page = await logseq.Editor.getPage(PAGE_NAME)
+  if (page) return
 
   await logseq.Editor.createPage(
     PAGE_NAME,
-    { "excalidraw-plugin-library": "true" },
-    { format: "markdown", redirect: false }
-  );
-  await logseq.Editor.appendBlockInPage(PAGE_NAME, EXCALIDRAW_FILE_PROMPT);
-  await logseq.Editor.appendBlockInPage(
-    PAGE_NAME,
-    genBlockData(DEFAULT_EXCALIDRAW_LIBRARY_ITEMS)
-  );
-};
+    { 'excalidraw-plugin-library': 'true' },
+    { format: 'markdown', redirect: false },
+  )
+  await logseq.Editor.appendBlockInPage(PAGE_NAME, EXCALIDRAW_FILE_PROMPT)
+  await logseq.Editor.appendBlockInPage(PAGE_NAME, genBlockData(DEFAULT_EXCALIDRAW_LIBRARY_ITEMS))
+}
 
 export const getExcalidrawLibraryItems = async () => {
-  const pageBlocks = await logseq.Editor.getPageBlocksTree(PAGE_NAME);
-  const codeBlock = pageBlocks?.[2];
-  const libraryItems = getExcalidrawData(codeBlock?.content) as LibraryItems;
-  return libraryItems;
-};
+  const pageBlocks = await logseq.Editor.getPageBlocksTree(PAGE_NAME)
+  const codeBlock = pageBlocks?.[2]
+  const libraryItems = getExcalidrawData(codeBlock?.content) as LibraryItems
+  return libraryItems
+}
 
 export const updateExcalidrawLibraryItems = async (items: LibraryItems) => {
-  const pageBlocks = await logseq.Editor.getPageBlocksTree(PAGE_NAME);
-  const codeBlock = pageBlocks?.[2];
-  return logseq.Editor.updateBlock(codeBlock.uuid, genBlockData(items));
-};
+  const pageBlocks = await logseq.Editor.getPageBlocksTree(PAGE_NAME)
+  const codeBlock = pageBlocks?.[2]
+  return logseq.Editor.updateBlock(codeBlock.uuid, genBlockData(items))
+}
 
-export default bootExcalidrawLibraryItems;
+export default bootExcalidrawLibraryItems
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,73 +1,54 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useToast } from "@/components/ui/use-toast";
-import {
-  Excalidraw,
-  Button,
-  MainMenu,
-  WelcomeScreen,
-} from "@excalidraw/excalidraw";
-import type { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
-import type { AppState, BinaryFiles } from "@excalidraw/excalidraw/types/types";
-import { debounce } from "lodash";
-import { TbLogout, TbBrandGithub, TbArrowsMinimize } from "react-icons/tb";
-import {
-  genBlockData,
-  getExcalidrawInfoFromPage,
-  getLangCode,
-  getMinimalAppState,
-  updateLogseqPageProperty,
-} from "@/lib/utils";
-import type { ExcalidrawData, PluginSettings } from "@/type";
-import type { LibraryItems } from "@excalidraw/excalidraw/types/types";
-import {
-  getExcalidrawLibraryItems,
-  updateExcalidrawLibraryItems,
-} from "@/bootstrap/excalidrawLibraryItems";
-import getI18N from "@/locales";
-import { Input } from "@/components/ui/input";
-import TagSelector from "./TagSelector";
-import { BlockIdentity } from "@logseq/libs/dist/LSPlugin";
+import { Excalidraw, Button, MainMenu, WelcomeScreen } from '@excalidraw/excalidraw'
+import type { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types'
+import type { AppState, BinaryFiles } from '@excalidraw/excalidraw/types/types'
+import type { LibraryItems } from '@excalidraw/excalidraw/types/types'
+import type { BlockIdentity } from '@logseq/libs/dist/LSPlugin'
+import { debounce } from 'lodash'
+import React, { useEffect, useRef, useState } from 'react'
+import { TbLogout, TbBrandGithub, TbArrowsMinimize } from 'react-icons/tb'
 
-export type Theme = "light" | "dark";
+import { getExcalidrawLibraryItems, updateExcalidrawLibraryItems } from '@/bootstrap/excalidrawLibraryItems'
+import { Input } from '@/components/ui/input'
+import { useToast } from '@/components/ui/use-toast'
+import { genBlockData, getExcalidrawInfoFromPage, getLangCode, getMinimalAppState } from '@/lib/utils'
+import getI18N from '@/locales'
+import type { ExcalidrawData, PluginSettings } from '@/type'
+
+import TagSelector from './TagSelector'
+
+export type Theme = 'light' | 'dark'
 export enum EditorTypeEnum {
-  App = "app",
-  Page = "page",
+  App = 'app',
+  Page = 'page',
 }
-const WAIT = 1000;
-const updatePageProperty = debounce(
-  (block: BlockIdentity, key: string, value: string) => {
-    logseq.Editor.upsertBlockProperty(block, key, value);
-  },
-  WAIT
-);
+const WAIT = 1000
+const updatePageProperty = debounce((block: BlockIdentity, key: string, value: string) => {
+  logseq.Editor.upsertBlockProperty(block, key, value)
+}, WAIT)
 
 const Editor: React.FC<
   React.PropsWithChildren<{
-    pageName: string;
-    onClose?: () => void;
-    type?: EditorTypeEnum;
+    pageName: string
+    onClose?: () => void
+    type?: EditorTypeEnum
   }>
 > = ({ pageName, onClose, type = EditorTypeEnum.App }) => {
-  const [excalidrawData, setExcalidrawData] = useState<ExcalidrawData>();
-  const [libraryItems, setLibraryItems] = useState<LibraryItems>();
-  const [theme, setTheme] = useState<Theme>();
-  const blockUUIDRef = useRef<string>();
-  const pagePropertyBlockUUIDRef = useRef<string>();
-  const currentExcalidrawDataRef = useRef<ExcalidrawData>();
+  const [excalidrawData, setExcalidrawData] = useState<ExcalidrawData>()
+  const [libraryItems, setLibraryItems] = useState<LibraryItems>()
+  const [theme, setTheme] = useState<Theme>()
+  const blockUUIDRef = useRef<string>()
+  const pagePropertyBlockUUIDRef = useRef<string>()
+  const currentExcalidrawDataRef = useRef<ExcalidrawData>()
 
-  const [aliasName, setAliasName] = useState<string>();
-  const [tag, setTag] = useState<string>();
+  const [aliasName, setAliasName] = useState<string>()
+  const [tag, setTag] = useState<string>()
 
-  const { toast } = useToast();
-  const { editor: i18nEditor } = getI18N();
+  const { toast } = useToast()
+  const { editor: i18nEditor } = getI18N()
 
   // save excalidraw data to currentExcalidrawDataRef
   const onExcalidrawChange = debounce(
-    (
-      elements: readonly ExcalidrawElement[],
-      appState: AppState,
-      files: BinaryFiles
-    ) => {
+    (elements: readonly ExcalidrawElement[], appState: AppState, files: BinaryFiles) => {
       // const blockData = genBlockData({
       //   ...excalidrawData,
       //   elements: excalidrawElements,
@@ -80,98 +61,85 @@ const Editor: React.FC<
         elements,
         appState,
         files,
-      };
+      }
     },
-    WAIT
-  );
+    WAIT,
+  )
   // save library items to page
   const onLibraryChange = (items: LibraryItems) => {
-    updateExcalidrawLibraryItems(items);
-  };
+    updateExcalidrawLibraryItems(items)
+  }
   // save excalidraw data to page
   const onClickClose = (type?: EditorTypeEnum) => {
     const { id, dismiss } = toast({
-      variant: "destructive",
+      variant: 'destructive',
       title: i18nEditor.saveToast.title,
       description: i18nEditor.saveToast.description,
       duration: 0,
-    });
+    })
     setTimeout(async () => {
       if (currentExcalidrawDataRef.current && blockUUIDRef.current) {
-        console.log("[faiz:] === start save");
-        const { elements, appState, files } = currentExcalidrawDataRef.current;
+        console.log('[faiz:] === start save')
+        const { elements, appState, files } = currentExcalidrawDataRef.current
         const blockData = genBlockData({
           ...excalidrawData,
           elements,
           appState: getMinimalAppState(appState!),
           files,
-        });
-        await logseq.Editor.updateBlock(blockUUIDRef.current, blockData);
-        console.log("[faiz:] === end save");
-        dismiss();
-        onClose?.();
+        })
+        await logseq.Editor.updateBlock(blockUUIDRef.current, blockData)
+        console.log('[faiz:] === end save')
+        dismiss()
+        onClose?.()
       }
-    }, WAIT + 100);
-  };
+    }, WAIT + 100)
+  }
 
   const onAliasNameChange = (aliasName: string) => {
-    setAliasName(aliasName);
+    setAliasName(aliasName)
     if (pagePropertyBlockUUIDRef.current) {
-      updatePageProperty(
-        pagePropertyBlockUUIDRef.current,
-        "excalidraw-plugin-alias",
-        aliasName
-      );
+      updatePageProperty(pagePropertyBlockUUIDRef.current, 'excalidraw-plugin-alias', aliasName)
     }
-  };
+  }
   const onTagChange = (tag: string) => {
-    setTag(tag);
+    setTag(tag)
     if (pagePropertyBlockUUIDRef.current) {
-      updatePageProperty(
-        pagePropertyBlockUUIDRef.current,
-        "excalidraw-plugin-tag",
-        tag
-      );
+      updatePageProperty(pagePropertyBlockUUIDRef.current, 'excalidraw-plugin-tag', tag)
     }
-  };
+  }
 
   // initialize excalidraw data
   useEffect(() => {
     getExcalidrawInfoFromPage(pageName).then((data) => {
-      setExcalidrawData(data?.excalidrawData);
-      blockUUIDRef.current = data?.block?.uuid;
+      setExcalidrawData(data?.excalidrawData)
+      blockUUIDRef.current = data?.block?.uuid
 
-      const firstBlock = data?.rawBlocks?.[0];
-      pagePropertyBlockUUIDRef.current = firstBlock?.uuid;
-      setAliasName(firstBlock.properties?.excalidrawPluginAlias || "");
-      setTag(firstBlock.properties?.excalidrawPluginTag?.toLowerCase?.() || "");
-    });
-  }, [pageName]);
+      const firstBlock = data?.rawBlocks?.[0]
+      pagePropertyBlockUUIDRef.current = firstBlock?.uuid
+      setAliasName(firstBlock.properties?.excalidrawPluginAlias || '')
+      setTag(firstBlock.properties?.excalidrawPluginTag?.toLowerCase?.() || '')
+    })
+  }, [pageName])
   // initialize library items
   useEffect(() => {
     getExcalidrawLibraryItems().then((items) => {
-      setLibraryItems(items || []);
-    });
-  }, []);
+      setLibraryItems(items || [])
+    })
+  }, [])
   // initialize theme
   useEffect(() => {
-    logseq.App.getStateFromStore<Theme>("ui/theme").then(setTheme);
-  }, []);
+    logseq.App.getStateFromStore<Theme>('ui/theme').then(setTheme)
+  }, [])
 
   return (
     <div className="w-screen h-screen">
       {excalidrawData && libraryItems && (
         <Excalidraw
-          langCode={getLangCode(
-            (logseq.settings as unknown as PluginSettings)?.langCode
-          )}
+          langCode={getLangCode((logseq.settings as unknown as PluginSettings)?.langCode)}
           initialData={{
             elements: excalidrawData.elements || [],
             appState: excalidrawData.appState
-              ? Object.assign(
-                  { theme },
-                  getMinimalAppState(excalidrawData.appState)
-                )
+              ? Object.assign({ theme }, getMinimalAppState(excalidrawData.appState))
               : { theme },
             files: excalidrawData?.files || undefined,
             libraryItems,
@@ -180,22 +148,14 @@ const Editor: React.FC<
           onLibraryChange={onLibraryChange}
           renderTopRightUI={() => (
             <>
-              <Input
-                placeholder="Untitled"
-                value={aliasName}
-                onChange={(e) => onAliasNameChange(e.target.value)}
-              />
+              <Input placeholder="Untitled" value={aliasName} onChange={(e) => onAliasNameChange(e.target.value)} />
               <TagSelector showAdd value={tag} onChange={onTagChange} />
               <Button
                 onSelect={() => onClickClose(type)}
-                style={{ width: "38px", height: "38px", color: "#666" }}
+                style={{ width: '38px', height: '38px', color: '#666' }}
                 title={i18nEditor.exitButton}
               >
-                {type === EditorTypeEnum.App ? (
-                  <TbLogout />
-                ) : (
-                  <TbArrowsMinimize />
-                )}
+                {type === EditorTypeEnum.App ? <TbLogout /> : <TbArrowsMinimize />}
               </Button>
             </>
           )}
@@ -203,11 +163,7 @@ const Editor: React.FC<
           <MainMenu>
             <MainMenu.Item
               icon={<TbBrandGithub />}
-              onSelect={() =>
-                logseq.App.openExternalLink(
-                  "https://github.com/haydenull/logseq-plugin-excalidraw"
-                )
-              }
+              onSelect={() => logseq.App.openExternalLink('https://github.com/haydenull/logseq-plugin-excalidraw')}
             >
               Github
             </MainMenu.Item>
@@ -221,15 +177,13 @@ const Editor: React.FC<
             <WelcomeScreen.Hints.ToolbarHint />
             <WelcomeScreen.Center>
               <WelcomeScreen.Center.Logo></WelcomeScreen.Center.Logo>
-              <WelcomeScreen.Center.Heading>
-                Logseq Excalidraw Plugin
-              </WelcomeScreen.Center.Heading>
+              <WelcomeScreen.Center.Heading>Logseq Excalidraw Plugin</WelcomeScreen.Center.Heading>
             </WelcomeScreen.Center>
           </WelcomeScreen>
         </Excalidraw>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Editor;
+export default Editor
